41b619fba78ef8715b3759e8230a149ffb356e99
JTC <joetcld@gmail.com>
2025-11-27 17:37:55 -0600

    Paso 4

---
+++ Main.java
@@ -0,0 +1,11 @@
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		Student joe = new Student ("Joe Torres", 85);
+		
+
+	}
+
+}
--- Student.java
+++ Student.java
@@ -1,9 +1,40 @@
+import java.util.HashMap;
 
 public class Student {
+	   
+	String firstName;
+    String lastName;
+    int registration;
+    int grade;
+    int year;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+ public Student (String firstName, String lastName, int registration, int grade, int year) {
+	this.firstName = firstName.toUpperCase();
+	this.lastName = lastName.toUpperCase();
+	this.registration = registration;
+	this.grade = grade;
+	this.year = year;
+}//constructor
+	public Student (String firstName, String lastName) {
+		this(firstName, lastName, 9876, 85, 3);
+	}//constructor (sobrecarga)
+	
+public void printFullName(){
+	System.out.println(firstName + " " + lastName);
+}
 
-	}
+ public void isApproved(){
+    if( grade >= 60) 
+    
+ }
+
+ public int changeYearIfApproved() {
+    if (isApproved()) {
+    	year = year + 1;
+    	System.out.println("Congratulations");
+    }
+     return 0;
+ }
+ 
 
 }
